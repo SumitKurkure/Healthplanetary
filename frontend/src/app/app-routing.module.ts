05bf24ca51660caf1299f458e8f75a2b3aa459bb
SumitKurkureKL <107413868+SumitKurkureKL@users.noreply.github.com>
2023-04-07 12:24:55 +0530

    initial setup

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { EditPostComponent } from './dashboard/edit-post/edit-post.component';
+import { NewPostComponent } from './dashboard/new-post/new-post.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      { path: 'post/new', component: NewPostComponent },
+      { path: 'post/edit', component: EditPostComponent },
+    ]
+  },
+  { path: 'post/:postId', component: ViewPostComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { EditPostComponent } from './dashboard/edit-post/edit-post.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewPostComponent } from './dashboard/new-post/new-post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LoginComponent,
+    NewPostComponent,
+    ViewPostComponent,
+    EditPostComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
